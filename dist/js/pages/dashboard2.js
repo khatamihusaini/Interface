c5f65b2439b6d0fc216d3a3c41b964270e889af6
khatamihusaini <38415616+khatamihusaini@users.noreply.github.com>
2018-08-08 10:08:29 +0700

    a

---
+++ dist/js/pages/dashboard2.js
@@ -0,0 +1,137 @@
+$(function () {
+
+  'use strict';
+
+  /* ChartJS
+   * -------
+   * Here we will create a few charts using ChartJS
+   */
+
+  // -----------------------
+  // - MONTHLY SALES CHART -
+  // -----------------------
+
+  // Get context with jQuery - using jQuery's .get() method.
+  var salesChartCanvas = $('#salesChart').get(0).getContext('2d');
+  // This will get the first returned node in the jQuery collection.
+  var salesChart       = new Chart(salesChartCanvas);
+
+  var salesChartData = {
+    labels  : ['Q1', 'Q2', 'Q3', 'Q4'],
+    datasets: [
+      {
+        label               : 'Revenue',
+        fillColor           : 'rgba(60,141,188,0.9)',
+        strokeColor         : 'rgba(60,141,188,0.8)',
+        pointColor          : '#3b8bba',
+        pointStrokeColor    : 'rgba(60,141,188,1)',
+        pointHighlightFill  : '#fff',
+        pointHighlightStroke: 'rgba(60,141,188,1)',
+        data                : [31.2, 33.2, 36.6, 44.2]
+      }
+    ]
+  };
+
+  var salesChartOptions = {
+    // Boolean - If we should show the scale at all
+    showScale               : true,
+    // Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines      : false,
+    // String - Colour of the grid lines
+    scaleGridLineColor      : 'rgba(0,0,0,.05)',
+    // Number - Width of the grid lines
+    scaleGridLineWidth      : 1,
+    // Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+    // Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines  : true,
+    // Boolean - Whether the line is curved between points
+    bezierCurve             : true,
+    // Number - Tension of the bezier curve between points
+    bezierCurveTension      : 0.3,
+    // Boolean - Whether to show a dot for each point
+    pointDot                : true,
+    // Number - Radius of each point dot in pixels
+    pointDotRadius          : 4,
+    // Number - Pixel width of point dot stroke
+    pointDotStrokeWidth     : 1,
+    // Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+    // Boolean - Whether to show a stroke for datasets
+    datasetStroke           : true,
+    // Number - Pixel width of dataset stroke
+    datasetStrokeWidth      : 2,
+    // Boolean - Whether to fill the dataset with a color
+    datasetFill             : true,
+    // String - A legend template
+    legendTemplate          : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].lineColor%>\'></span><%=datasets[i].label%></li><%}%></ul>',
+    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
+    maintainAspectRatio     : true,
+    // Boolean - whether to make the chart responsive to window resizing
+    responsive              : true
+  };
+  // Create the line chart
+  salesChart.Line(salesChartData, salesChartOptions);
+
+  // ---------------------------
+  // - END MONTHLY SALES CHART -
+  // ---------------------------
+
+
+  //-------------
+    //- BAR CHART -
+    //-------------
+    var barChartCanvas                   = $('#barChart').get(0).getContext('2d')
+    var barChart                         = new Chart(barChartCanvas)
+    var barChartData                     = {
+      labels  : ['Mei 2018', 'Juni 2018'],
+      datasets: [
+        
+        {
+          label               : 'Digital Goods',
+          fillColor           : 'rgba(60,141,188,0.9)',
+          strokeColor         : 'rgba(60,141,188,0.8)',
+          pointColor          : '#3b8bba',
+          pointStrokeColor    : 'rgba(60,141,188,1)',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(60,141,188,1)',
+          data                : [11.3, 11.7]
+        }
+      ]
+    }
+    barChartData.datasets[0].fillColor   = '#00a65a'
+    barChartData.datasets[0].strokeColor = '#00a65a'
+    barChartData.datasets[0].pointColor  = '#00a65a'
+    var barChartOptions                  = {
+      //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+      scaleBeginAtZero        : true,
+      //Boolean - Whether grid lines are shown across the chart
+      scaleShowGridLines      : true,
+      //String - Colour of the grid lines
+      scaleGridLineColor      : 'rgba(0,0,0,.05)',
+      //Number - Width of the grid lines
+      scaleGridLineWidth      : 1,
+      //Boolean - Whether to show horizontal lines (except X axis)
+      scaleShowHorizontalLines: true,
+      //Boolean - Whether to show vertical lines (except Y axis)
+      scaleShowVerticalLines  : true,
+      //Boolean - If there is a stroke on each bar
+      barShowStroke           : true,
+      //Number - Pixel width of the bar stroke
+      barStrokeWidth          : 2,
+      //Number - Spacing between each of the X value sets
+      barValueSpacing         : 5,
+      //Number - Spacing between data sets within X values
+      barDatasetSpacing       : 1,
+      //String - A legend template
+      legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+      //Boolean - whether to make the chart responsive
+      responsive              : true,
+      maintainAspectRatio     : true
+    }
+
+    barChartOptions.datasetFill = false
+    barChart.Bar(barChartData, barChartOptions)
+
+
+});
